af3208a1d1787abc2e83b6dd3443635f1313b1b6
Spencer Foote <83970495+SJFoote@users.noreply.github.com>
2021-06-27 13:42:25 -0700

    beautiful soup upload

--- shop_assist/shop_assist_app/rainForest.py
+++ shop_assist/shop_assist_app/rainForest.py
@@ -1,5 +1,7 @@
 import json
 import requests
+from bs4 import BeautifulSoup
+from lxml import etree
 
 # set up the request parameters
 # params = {
@@ -16,22 +18,35 @@ import requests
 
 
 # set up the request parameters
-params = {
-    'api_key': '2613DE212DD24509A46720C4C27202D9',
-    'type': 'bestsellers',
-    'url': 'https://www.amazon.com/Best-Sellers-Musical-Instruments/zgbs/musical-instruments/ref=zg_bs_nav_0'
-}
+# params = {
+#     'api_key': '2613DE212DD24509A46720C4C27202D9',
+#     'type': 'bestsellers',
+#     'url': 'https://www.amazon.com/Best-Sellers-Musical-Instruments/zgbs/musical-instruments/ref=zg_bs_nav_0'
+# }
 
-# make the http GET request to Rainforest API
-api_result = requests.get('https://api.rainforestapi.com/request', params)
+# # make the http GET request to Rainforest API
+# api_result = requests.get('https://api.rainforestapi.com/request', params)
 
-data = json.dumps(api_result.json(), indent=4, sort_keys=True)
+# data = json.dumps(api_result.json(), indent=4, sort_keys=True)
 
-# print the JSON response from Rainforest API
-print(data)
+# # print the JSON response from Rainforest API
+# print(data)
 
 
-# print(data['bestsellers'])
+# # print(data['bestsellers'])
 
-# for item in data['type']:
-#     print (item["current_category"])
+# # for item in data['type']:
+# #     print (item["current_category"])
+
+
+URL = 'https://books.toscrape.com/?'
+page = requests.get(URL)
+
+# soup = BeautifulSoup(page.content, 'html.parser')
+# dom = etree.HTML(str(soup))
+# print(dom.xpath('//*[@id="mainContainer"]/div[5]/div/div[2]/div/div[1]/button/div[2]/div/p')[0].text)
+# print(soup.find_all("h3")[0].text)
+soup = BeautifulSoup(page.text, 'lxml')
+print("List of all books:")
+for heading in soup.select('a[href*="catalogue"]'):
+    print(heading.get('title'))
