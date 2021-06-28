36067df85c8a091c33a193199901ddbf7b0ae14e
JakeZ34 <JzBaseball34@hotmail.com>
2021-06-27 22:52:21 -0700

    ?

--- shop_assist/shop_assist_app/models.py
+++ shop_assist/shop_assist_app/models.py
@@ -8,6 +8,8 @@ URL = 'https://books.toscrape.com/?'
 page = requests.get(URL)
 
 # Create your models here.
+
+
 class BookManager(models.Manager):
     def getTitles(request):
         soup = BeautifulSoup(page.text, 'lxml')
@@ -18,8 +20,30 @@ class BookManager(models.Manager):
                 bookList.append(bookTitle)
         return bookList
 
-class Book(models.Model): 
-    title = models.CharField(max_length = 255)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    def getPrice(request):
+        #1
+        soup = BeautifulSoup(page.text, 'lxml')
+        bookList = []
+        for heading in soup.select('a[href*="catalogue"]'):
+            bookPrice = heading.get('price')
+            print(soup.price)
+        return bookList
+        #2
+        soup = BeautifulSoup(page.text, 'lxml')
+        bookList = []
+        for price in soup.find_all('price'):
+            print(price.string)
+            print(str(price.text))
+        return bookList
+        #3
+        for price in soup.find_all('price'):
+            print(soup.get('price'))
+        return bookList
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    #price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     objects = BookManager()
