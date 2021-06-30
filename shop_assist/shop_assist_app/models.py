b26c946786b99e82d0a404e8d3ce95c4a2012f1f
Spencer Foote <83970495+SJFoote@users.noreply.github.com>
2021-06-30 16:46:49 -0700

    setup the rest of the books manager and passed info to html

--- shop_assist/shop_assist_app/models.py
+++ shop_assist/shop_assist_app/models.py
@@ -6,6 +6,7 @@ from lxml import etree
 
 URL = 'https://books.toscrape.com/?'
 page = requests.get(URL)
+soup = BeautifulSoup(page.text, 'lxml')
 
 # Create your models here.
 class BookManager(models.Manager):
@@ -17,6 +18,19 @@ class BookManager(models.Manager):
             if bookTitle != None:
                 bookList.append(bookTitle)
         return bookList
+
+    def getPrice(request):
+        bookPrice = []
+        for price in soup.select('p[class*="price_color"]'):
+            bookPrice.append(price.get_text())
+        return bookPrice
+
+    def getImage(request):
+        bookImage = []
+        for img in soup.findAll('div', {'class': 'image_container'}):
+            for image in img.findAll('img',src=True):
+                bookImage.append(image['src'])
+        return bookImage
 
 class Book(models.Model): 
     title = models.CharField(max_length = 255)
